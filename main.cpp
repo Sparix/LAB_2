257dab77b1b56a9808540b8dc4a31abf2bbb0af7
Sparix <54955798+Sparix@users.noreply.github.com>
2021-12-23 11:08:15 +0200

    lab2

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include "calculator.h"
 
-
-
 int main()
 {
     Calculator calc;
-    std::cout << calc.Add(5, 7) << '\n';
 
+    std::cout << calc.Sub(calc.Mul(9, 8), calc.Add(1, 2)) << '\n';
 
     return 0;
 }
